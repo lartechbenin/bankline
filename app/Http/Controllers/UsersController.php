ebde9e7f28d51c424a225f567b73730f9529ac95
Zinsou théagnorax GANDAHO <gandaho.letolerent@gmail.com>
2022-12-10 13:50:33 +0100

    ajout des champs pays et sexe dans le fichier user du dossier migration

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,12 +8,7 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    public function index(){
-
-        
-        
-
-        
+    public function index(){        
         
         return view('index');
     }
