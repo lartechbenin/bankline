baeeddb5000444c3526e059d614983c05361f411
Zinsou théagnorax GANDAHO <gandaho.letolerent@gmail.com>
2022-12-11 09:30:52 +0100

    creation de composant livewire pour l'inscription

---
+++ app/Http/Livewire/Livewiresignup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Livewiresignup extends Component
+{
+    public function render()
+    {
+        return view('livewire.livewiresignup');
+    }
+}
--- resources/views/inscription.blade.php
+++ resources/views/inscription.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+
     <meta charset="utf-8">
     <title>inscription</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
@@ -29,9 +30,12 @@
 
     <!-- Template Stylesheet -->
     <link href="css/style.css" rel="stylesheet">
+    <script defer src="https://unpkg.com/alpinejs@3.1.1/dist/cdn.min.js"></script>
 </head>
 
 <body>
+
+
     <div class="container-xxl position-relative bg-white d-flex p-0">
         <!-- Spinner Start -->
         <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
@@ -56,90 +60,106 @@
                         <hr></hr>
                     <div >    <center ><h5 class="text-danger">Informations personnelles</h5></center></div>
                         <hr></hr>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="Nom de la famille ">
-                            <label for="floatingText">Nom de la famille</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="Prenom ">
-                            <label for="floatingText">Prenom</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="Pays">
-                            <label for="floatingText">Pays</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Email address</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="tel" class="form-control" id="floatingText" placeholder="Telepgone">
-                            <label for="floatingText">Telephone</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="date" class="form-control" id="floatingText" placeholder="Date de naissance ">
-                            <label for="floatingText">Date de naissance</label>
-                        </div>
-                        <hr></hr>
-                        <div >    <center ><h5 class="text-danger">informations complémentaires </h5><h6>(facultatif)</h6></center></div>
 
-                        <hr></hr>
-                        <div class="form-floating mb-3">
-                        <br><br>
-                            <input type="text" class="form-control" id="floatingText" placeholder="Nationnalité">
-                            <label for="floatingText">Veuillez nous dire quelle est votre nationalité et votre lieu de naissance.</label>
-                        </div>
+                        <form method="post" wire:submit.prevent="inscription()">
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="nom" placeholder="Nom de la famille " wire:model.lazy="nom">
+                                    <label for="nom">Nom de la famille</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="prenom" placeholder="Prenom " wire:model.lazy="prenom">
+                                    <label for="prenom">Prenom</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="pays" placeholder="Pays" wire:model.lazy="pays">
+                                    <label for="pays">Pays</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="email" class="form-control" id="email" placeholder="name@example.com" wire:model.lazy="email">
+                                    <label for="email">Email address</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="tel" class="form-control" id="telephone" placeholder="Telepgone" wire:model.lazy="telephone">
+                                    <label for="telephone">Telephone</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="date" class="form-control" id="date_naissance" placeholder="Date de naissance" wire:model.lazy="date_naissance">
+                                    <label for="date_naissance">Date de naissance</label>
+                                </div>
+                                <hr></hr>
+                                <div >    <center ><h5 class="text-danger">informations complémentaires </h5><h6>(facultatif)</h6></center></div>
 
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="ville de naissance ">
-                            <label for="floatingText">ville de naissance</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="Pays de naissance ">
-                            <label for="floatingText">Pays de naissance</label>
-                        </div>
-                        <fieldset class="row mb-3">
-                                    <legend class="col-form-label col-sm-2 pt-0">Sexe</legend>
-                                    <div class="col-sm-10">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="gridRadios"
-                                                id="gridRadios1" value="option1" checked>
-                                            <label class="form-check-label" for="gridRadios1">
-                                                Homme
-                                            </label>
+                                <hr></hr>
+                                <div class="form-floating mb-3">
+                                <br><br>
+                                    <input type="text" class="form-control" id="natialite" placeholder="Nationnalité">
+                                    <label for="nationalite">Veuillez nous dire quelle est votre nationalité et votre lieu de naissance.</label>
+                                </div>
+
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="ville" placeholder="ville de naissance ">
+                                    <label for="ville">ville de naissance</label>
+                                </div>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="paysnaissance" placeholder="Pays de naissance ">
+                                    <label for="paysnaissance">Pays de naissance</label>
+                                </div>
+                                <fieldset class="row mb-3">
+                                            <legend class="col-form-label col-sm-2 pt-0">Sexe</legend>
+                                            <div class="col-sm-10">
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="gridRadios"
+                                                        id="homme"  checked wire:model="sexe" value="M">
+                                                    <label class="form-check-label" for="homme">
+                                                        Homme
+                                                    </label>
+                                                </div>
+                                                <div class="form-check">
+                                                    <input class="form-check-input" type="radio" name="gridRadios"
+                                                        id="femme" value="F" wire:model="sexe">
+                                                    <label class="form-check-label" for="femme">
+                                                    Femme
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </fieldset>
+                                <hr></hr>
+                                <div >    <center ><h5 class="text-danger">informations de Sécurité</h5></center></div>
+
+                                <hr></hr>
+                                <div class="form-floating mb-3">
+                                    <input type="text" class="form-control" id="numeropiece" placeholder="Numéro de votre pièce à founir" wire:model.lazy="numero_carte">
+                                    <label for="numeropiece">Numéro de votre pièce à founir(pièce d'identité, permis de conduis ect...)</label>
+                                </div>
+                                <div x-data="{
+                                    type: 'password',
+                                    statut: false,
+                                    texte: 'Afficher le mot de passe',
+                                    afficher: function afficher(){
+                                        this.statut = !this.statut;
+
+                                        this.type = this.statut ? 'text' : 'password';
+
+                                        this.texte = this.statut ? 'Masquer le mot de passe' : 'Afficher le mot de passe';
+                                    }
+                                }">
+                                        <div class="form-floating mb-4">
+                                            <input x-bind:type="type" class="form-control" id="password_confirmation" placeholder="Password" wire:model.lazy="password_confirmation">
+                                            <label for="password_confirmation">Password</label>
                                         </div>
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="radio" name="gridRadios"
-                                                id="gridRadios2" value="option2">
-                                            <label class="form-check-label" for="gridRadios2">
-                                               Femme
-                                            </label>
+                                        <div class="form-floating mb-4">
+                                            <input x-bind:type="type" class="form-control" id="password" placeholder="Entré à nouveau mot de passe" wire:model.lazy="password">
+                                            <label for="password">Entré à nouveau mot le passe</label>
                                         </div>
-                                    </div>
-                                </fieldset>
-                        <hr></hr>
-                        <div >    <center ><h5 class="text-danger">informations de Sécurité</h5></center></div>
-
-                        <hr></hr>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="Numéro de votre pièce à founir">
-                            <label for="floatingText">Numéro de votre pièce à founir(pièce d'identité, permis de conduis ect...)</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Password</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Entré à nouveau mot de passe">
-                            <label for="floatingPassword">Entré à nouveau mot le passe</label>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Afficher mot de passe</label>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Creer un compte</button>
+                                        <div class="d-flex align-items-center justify-content-between mb-4">
+                                            <div class="form-check">
+                                                <input type="checkbox" class="form-check-input" id="afficherpass" x-on:click="afficher">
+                                                <label class="form-check-label" for="afficherpass" x-text="texte"></label>
+                                            </div>
+                                        </div>
+                                </div>
+                                <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Creer un compte</button>
+                        </form>
                         <p class="text-center mb-0">Avez vous déja un compte? <a href="{{route('connection')}}">connexion</a></p>
                         <marquee class="text-danger">Toutes les informations sur ce site demeurent confidentielles et ne seront jamais révélées.</marquee>
                     </div> 
